f6c50630969e72a8fa04d71d7aefca01fde51f8f
Piotr Pańko <pankopiotr1@gmail.com>
2018-06-27 21:11:23 +0200

    Add complete task method

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   belongs_to :todolist
   validates_presence_of :title
   validates :completed, :inclusion => {:in => [true, false]}
+
+  def complete
+    update(completed: true)
+  end
 end
