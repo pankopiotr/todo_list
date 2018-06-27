de5e779b7ba3165d7a512d250144fea254146bc7
Piotr Pańko <pankopiotr1@gmail.com>
2018-06-27 20:07:15 +0200

    Add deadline to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180627175555) do
+ActiveRecord::Schema.define(version: 20180627180632) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180627175555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false, null: false
+    t.date "deadline"
     t.index ["todolist_id"], name: "index_tasks_on_todolist_id"
   end
 
