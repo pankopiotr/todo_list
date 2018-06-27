873fb20101bb809e345411001e412ed56cca9ade
Piotr Pańko <pankopiotr1@gmail.com>
2018-06-27 19:57:56 +0200

    Add completed field to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212181717) do
+ActiveRecord::Schema.define(version: 20180627175412) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180212181717) do
     t.integer "todolist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false, null: false
     t.index ["todolist_id"], name: "index_tasks_on_todolist_id"
   end
 
